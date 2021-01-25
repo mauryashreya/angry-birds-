7a47f59e9542c80ec99e046e99d2005d669e1e47
shreya maurya <mauryashreya64@gmail.com>
2021-01-25 14:40:22 +0530

    added a image

--- Ground.js
+++ Ground.js
@@ -4,14 +4,16 @@ class Ground {
           isStatic: true
       }
       this.body = Bodies.rectangle(x,y,width,height,options);
+      this.image=loadImage("sprites/ground.png");
       this.width = width;
       this.height = height;
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
-      rectMode(CENTER);
-      fill("brown");
-      rect(pos.x, pos.y, this.width, this.height);
+      push();
+      imageMode(CENTER);
+      image(this.image,pos.x,pos.y,this.width,this.height);
+      pop();
     }
   };
